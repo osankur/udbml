8422e5d1b414ee3a5ca6813e064fb55ea7a8c5bd
Maximilien Colange <colange@lsv.fr>
2015-12-02 16:15:20 +0100

    Make closure inclusion test visible (to be used from the priced zone part).

--- src/udbm_stubs.h
+++ src/udbm_stubs.h
@@ -8,4 +8,8 @@ typedef std::vector<int> carray_t;
 
 #define get_dbm_ptr(x) static_cast<dbm_t*>(Data_custom_val(x))
 
+bool
+dbm_closure_leq(const raw_t * const dr1, const raw_t * const dr2, cindex_t dim,
+                const std::vector<int> &lbounds, const std::vector<int> &ubounds);
+
 #endif  // UDBM_STUBS_H_
